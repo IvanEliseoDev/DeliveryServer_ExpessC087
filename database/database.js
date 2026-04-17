d33137fdb5094834908d441a11d6adde17098a54
ivan Hernandez <ivaneliseodev@gmail.com>
2026-04-17 13:52:33 -0600

    feat: Mongoose Connection in Database.js

---
+++ database/database.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require("mongoose");
+
+mongoose.connect()
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("DB IS CONNECTED")
+})
+connection.on("disconnected", () => {
+    console.log("DB is disconnecteds")
+})
+connection.on("error", (error) => {
+    console.log("ERROR found: ", error)
+})
